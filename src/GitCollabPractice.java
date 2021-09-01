fc50c276f8f4061eec79fb53b52a6ac5d674680d
bledi <bledi.bllaca12@gmail.com>
2021-09-01 13:09:46 +0200

    testr

--- src/GitCollabPractice.java
+++ src/GitCollabPractice.java
@@ -1,7 +1,7 @@
 public class GitCollabPractice {
     public static void main(String[] args) {
         System.out.println("f");
-        new Exception();
+        new String();
         //hej test
     }
 }
